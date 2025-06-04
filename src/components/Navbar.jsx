3525776ceb95db63adfbe894f0ef305006639825
Andriy <andygrigs88@gmail.com>
2025-06-04 08:32:39 +0200

    vars css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,28 @@ const Navbar = () => {
     <nav>
         <div className="container mx-auto px-6 py-4 flex justify-between items-center">
             <NavLink to='/'><Logo/></NavLink>
+              <div className="space-x-4">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive
+                ? 'text-blue-600 font-semibold'
+                : 'text-gray-700 hover:text-blue-600'
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="/catalog"
+            className={({ isActive }) =>
+              isActive
+                ? 'text-blue-600 font-semibold'
+                : 'text-gray-700 hover:text-blue-600'
+            }
+          >
+            Catalog
+          </NavLink>
+        </div>
         </div>
     </nav>
   )
