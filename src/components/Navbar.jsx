76dac00d3de3094d86cfa8c51339be093c465527
Andriy <andygrigs88@gmail.com>
2025-06-09 07:38:14 +0200

    fix navbar links in the center

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,16 @@ import Logo from "../shared/Logo";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="container mx-auto px-[64px] py-[24px] flex justify-between items-center">
-        <NavLink to="/">
-          <Logo className='w-[136px] h-[16px]'/>
+    <nav className='relative'>
+      <div className="container mx-auto px-[64px] py-[24px] flex justify-center items-center ">
+
+         <NavLink to="/" className="absolute left-6 top-1/2 -translate-y-1/2">
+          <div className="w-[136px] h-[16px]">
+            <Logo />
+          </div>
         </NavLink>
-        <div className="space-x-4">
+
+        <div className="space-x-6 flex">
           <NavLink
             to="/"
             className={({ isActive }) =>
